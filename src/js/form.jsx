7328ef671d08c8534c18f3bd9ecf72222605c5a4
mahistre <lionel.mahistre@radiofrance.com>
2018-08-07 18:39:48 +0200

    migration to react

--- src/js/form.jsx
+++ src/js/form.jsx
@@ -1,51 +1,46 @@
 
 const React = require('react');
-const $ = require('jquery');
+const jQuery = require('jquery');
 
 class Form extends React.Component {
 
+	constructor() {
+		super();
+		this.state = {
+			progress : 0,
+		}
+	}
+
 	upload(event) {
-		var files = $(event.target).get(0).files;
+		const self = this;
+		const files = event.target.files;
 
 		if (files.length > 0){
-			var formData = new FormData();
-
-			// loop through all the selected files and add them to the formData object
-			for (var i = 0; i < files.length; i++) {
-				var file = files[i];
+			const formData = new FormData();
+			for (let i = 0; i < files.length; i++) {
+				const file = files[i];
 				formData.append('uploads[]', file, file.name);
 			}
 
-			$.ajax({
+			jQuery.ajax({
 				url: '/upload',
 				type: 'POST',
 				data: formData,
 				processData: false,
 				contentType: false,
 				success: function(data){
-						console.log('upload successful!\n' + data);
+					console.log('upload successful!\n' + data);
 				},
 				xhr: function() {
-					var xhr = new XMLHttpRequest();
-
-					// listen to the 'progress' event
+					const xhr = new XMLHttpRequest();
 					xhr.upload.addEventListener('progress', function(evt) {
-
 						if (evt.lengthComputable) {
-							// calculate the percentage of upload completed
-							var percentComplete = evt.loaded / evt.total;
+							let percentComplete = evt.loaded / evt.total;
 							percentComplete = parseInt(percentComplete * 100);
-
-							// update the Bootstrap progress bar with the new percentage
-							$('.progress-bar').text(percentComplete + '%');
-							$('.progress-bar').width(percentComplete + '%');
-
-							if (percentComplete === 100) {
-								$('.progress-bar').html('Done');
-							}
-
+							self.setState({
+								progress : percentComplete,
+							});
 						}
-
 					}, false);
 					return xhr;
 				}
@@ -56,20 +51,28 @@ class Form extends React.Component {
 
 	triggerUpload() {
 		document.getElementById('upload-input').click();
-		$('.progress-bar').text('0%');
-		$('.progress-bar').width('0%');
+		this.setState({
+			progress : 0,
+		});
 	}
 
 
 	render() {
+		const progressLabel = this.state.progress < 100 ? this.state.progress+'%' : 'Done';
 		return (
-			<div className="panel-body">
-				<h2>File Uploader</h2>
-				<div className="progress">
-					<div className="progress-bar" role="progressbar"></div>
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="panel panel-default">
+						<div className="panel-body">
+							<h2>{"File Uploader"}</h2>
+							<div className="progress">
+								<div className="progress-bar" role="progressbar" style={{width : this.state.progress+'%'}}>{progressLabel}</div>
+							</div>
+							<button className="btn btn-lg upload-btn" type="button" onClick={this.triggerUpload.bind(this)}>{"Upload File"}</button>
+							<input id="upload-input" type="file" name="uploads[]" multiple="multiple " onChange={this.upload.bind(this)} />
+						</div>
+					</div>
 				</div>
-				<button className="btn btn-lg upload-btn" type="button" onClick={this.triggerUpload}>Upload File</button>
-				<input id="upload-input" type="file" name="uploads[]" multiple="multiple " onChange={this.upload.bind(this)} />
 			</div>
 		);
 	}
